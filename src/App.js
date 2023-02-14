09775dd4b1e55b9464aea63b790af0d02588a643
Maharshi <mallickmaharshi@gmail.com>
2023-02-15 00:43:54 +0530

    Top Navbar

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import React from 'react';
 import LoginPage from './loginpage';
 import RegisterPage from './Registerpage';
 import FileUploaderMod from './fileUploader';
+import TopToolbar from './topToolbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 const App = () => {
   return (
-    <Router>
-    <div className="App" style={{ backgroundImage: "url(./circle.jpg)" }}>
-      
-      <Routes>
-        <Route exact path="/Login" element={<LoginPage/>} />
-        <Route path="/Register" element={<RegisterPage/>} />
-        <Route path="/" element={<FileUploaderMod/>} />
-       
-      </Routes>
-    </div>
-    
-  </Router>
+    <>
+      <TopToolbar></TopToolbar>
+      <Router>
+        <div className="App" style={{ backgroundImage: "url(./circle.jpg)" }}>
+
+          <Routes>
+            <Route exact path="/Login" element={<LoginPage />} />
+            <Route path="/Register" element={<RegisterPage />} />
+            <Route path="/" element={<FileUploaderMod />} />
+
+          </Routes>
+        </div>
+
+      </Router>
+    </>
+
   );
 };
 
---
+++ src/topToolbar.js
@@ -0,0 +1,37 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
+
+import "./index.css"
+
+function topToolbar() {
+  return (
+    <Navbar bg="light" className="navBarStyle" expand="lg">
+      <Container>
+        {/* <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav></Nav>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Settings</Nav.Link>
+            <Nav.Link href="/Login">Login</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown> */}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default topToolbar;
